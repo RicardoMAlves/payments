3275765044209549b69e38632739f9a21cdd54aa
RicardoMAlves <ricardo.machalv@gmail.com>
2019-06-07 16:47:27 -0300

    Payments versao 1 - Adicionadas as classes Department e HourContract

---
+++ Payments/Enities/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace Payments.Enities
+{
+    class Department
+    {
+        public string name { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
---
+++ Payments/Enities/HourContract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payments.Enities
+{
+    class HourContract
+    {
+        public DateTime date { get; set; }
+        public double valuePerHour { get; set; }
+        public int hours { get; set; }
+
+        public double totalValue()
+        {
+            return valuePerHour * hours;
+        }
+
+        public override string ToString()
+        {
+            return date + ", " + valuePerHour.ToString("F2", CultureInfo.InvariantCulture) + ", " + hours;
+        }
+    }
+}
